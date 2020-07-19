f23c206dce9d358c9c6969e2ae50f945bb554692
Dani Melo <daniel.themoteo@gmail.com>
2020-07-19 20:46:13 -0300

    Fale Conosco

--- src/views/Principal.js
+++ src/views/Principal.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
+
 // import Navbar from '../components/menus/Navbar';
 // import Inicio from './Inicio/Inicio';
+import FaleConosco from './FaleConosco/FaleConosco';
+
+
 
 export default function Principal() {
   return (
     <div>
       {/* <Inicio id="inicio" /> */}
       {/* <Navbar /> */}
+
+      
+      <FaleConosco/>
+      
+      
     </div>
   );
 }
